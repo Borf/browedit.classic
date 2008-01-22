fefce89a504c28a7e49cdcf0942f93b8f738fd52
Mark Karpeles <MagicalTux>
2008-01-22 17:40:51 +0000

    Fixed(?): Dropped definition for int64_t and uint64_t for compiler compat.

--- src/common.h
+++ src/common.h
@@ -32,8 +32,6 @@ typedef unsigned short int uint16_t;
 typedef signed short int int16_t;
 typedef unsigned int uint32_t;
 typedef signed int int32_t;
-typedef unsigned long long int uint64_t;
-typedef signed long long int int64_t;
 
 #if __WORDSIZE == 64
 typedef uint64_t uintptr_t;
