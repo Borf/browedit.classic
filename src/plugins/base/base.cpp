eaa290e7402e84741013f15507d332fb3c7f7f59
Johan Talboom <borfje@gmail.com>
2008-12-10 11:19:31 +0000

    Fixed: line-endings are now fixed by svn

--- src/plugins/base/base.cpp
+++ src/plugins/base/base.cpp
@@ -3,12 +3,12 @@
 #include <wm/wm.h>
 #include <graphics.h>
 
-cPluginBase::cPluginBase(char* n, char* m)
+cPluginBase::cPluginBase(std::string n, std::string m)
 {
 	ZeroMemory(name, 128);
 	ZeroMemory(menu, 512);
-	strcpy(name,n);
-	strcpy(menu,m);
+	strcpy(name,n.c_str());
+	strcpy(menu,m.c_str());
 }
 
 void cPluginBase::setInterface(cBrowInterface* pInterface)
